11ce9cabba020223130df2cf343e22825fa644fc
Larissa Lira <LarissaLira627@gmail.com>
2026-06-08 23:49:16 -0300

    fix sqlite build

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
 // BANCO DE DADOS — SQLite
 // Cria as tabelas automaticamente se não existirem
 // =============================================
-const Database = require('better-sqlite3');
+const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 // Cria (ou abre) o arquivo do banco de dados
